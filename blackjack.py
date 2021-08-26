5c2d2438b0d54df6d6f5e60c2e8190f205c8dc6a
Rafael Llopis <rafa94el@gmail.com>
2021-08-26 22:15:59 +0200

    Implemented Play Again function

--- blackjack.py
+++ blackjack.py
@@ -1,20 +1,41 @@
 import random
 
-dealers_hand = []
-users_budget = 100
 cards = ['A', 'A', 'A', 'A', 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 'J', 'J', 'J', 'J', 'Q', 'Q', 'Q', 'Q', 'K', 'K', 'K', 'K']
 
 def NewGame():
+
+	global budget
+	global bet
+
 	while True:
-		user_bet = int(input("Your budget is {}. How much are you willing to bet? ".format(users_budget)))
-		if user_bet <= users_budget:
+		users_budget = int(input("What is your budget? "))
+		users_bet = int(input("Your budget is {}. How much are you willing to bet? ".format(users_budget)))
+		if users_bet <= users_budget:
 			break
 		else:
 			print("Your bet is higher than your budget!")
 			continue
-	global bet
-	bet = user_bet
+
+	bet = users_bet
+	budget = users_budget
 	new_game = Game()
+
+
+def PlayAgain():
+
+	global budget
+
+	while True:
+		users_bet = int(input("Your budget is {}. How much are you willing to bet? ".format(budget)))
+		if users_bet <= budget:
+			break
+		else:
+			print("Your bet is higher than your budget!")
+			continue
+
+	global bet
+	bet = users_bet
+	new_game = Game()	
 
 
 class Game:
@@ -28,6 +49,7 @@ class Game:
 		self.evaluate()
 
 	def evaluate(self):
+		global budget
 		self.users_score = 0
 
 		for i in self.users_hand:
@@ -44,17 +66,15 @@ class Game:
 		print("Your score is {}".format(self.users_score))		
 
 		if self.users_score == 21:
-			if self.count == 1:
-				print("Blackjack! You win {}".format(bet * 1.5))
-				self.users_budget += (bet * 1.5)
-				return
-			else:
-				self.users_budget += (bet * 2)
-				return
+			if self.count == 0:
+				budget += (bet * 1.5)
+				print("Blackjack! You win {}. Your budget is now {}".format(bet * 1.5, budget))
+				self.reset()
 
 		if self.users_score > 21:
-			print("You go bust! You lose {}".format(bet))
-			return
+			budget -= bet
+			print("You go bust! You lose {}. Your budget is now {}".format(bet, budget))
+			self.reset()
 
 		self.count += 1	
 		self.decide()
@@ -64,7 +84,7 @@ class Game:
 		self.evaluate()
 
 	def decide(self):
-		hit_or_stay = input("Do you want to hit or stay? (H/S) ")
+		hit_or_stay = input("Do you want to hit or stay? (H/S) ").upper()
 		if(hit_or_stay == 'H'):
 			self.deal()
 		else:
@@ -76,6 +96,7 @@ class Game:
 		self.evaluate_dealers()
 
 	def evaluate_dealers(self):
+		global budget
 		self.dealers_score = 0
 		for i in self.dealers_hand:
 			if i in ['J', 'Q', 'K']:
@@ -89,29 +110,42 @@ class Game:
 				self.dealers_score += i
 		print("The dealer's score is {}".format(self.dealers_score))		
 		if self.dealers_score == self.users_score:
-			print("It's a tie! You get your bet back.")
-			return
+			print("It's a tie! You get your bet back. Your budget is {}".format(budget))
+			self.reset()
 		elif self.dealers_score <= 16:
 			self.dealers_turn()
-		elif self.dealers_score > 16 <= 21:
+		elif self.dealers_score > 16 and self.dealers_score <= 21:
 			if self.dealers_score < self.users_score:
-				print("You win {}".format(bet * 2))
-				return
+				budget += bet * 2
+				print("You win {}. Your budget is now {}".format(bet * 2, budget))
+				self.reset()
 			else:
-				print("You lose your bet ({})".format(bet))
-				return
+				budget -= bet
+				print("You lose your bet ({}). Your budget is now {}".format(bet, budget))
+				self.reset()
 		else:
-			print("The dealer went bust. You win {}".format(bet * 2))
-			return
+			budget += bet * 2
+			print("The dealer went bust. You win {}. Your budget is now {}".format(bet * 2, budget))
+			self.reset()
 
 	def reset(self):
-		self.users_hand = random.choices(cards, k=2)
-		self.dealers_hand = random.choices(cards, k=1)
-		self.count = 0
-		print(self.users_hand)
-		print(self.dealers_hand)
-		NewGame()
+		end_game = input("What do you want to do now? \n Bet again (B). Quit (Q). Reset game (R)").upper()
+		global budget
 
+		if end_game == 'B':
+			self.users_hand = random.choices(cards, k=2)
+			self.dealers_hand = random.choices(cards, k=1)
+			self.count = 0
+			PlayAgain()
+		elif end_game == 'Q':
+			print("Thank you for playing Blackjack.")
+			return
+		else:
+			budget = 0
+			self.users_hand = random.choices(cards, k=2)
+			self.dealers_hand = random.choices(cards, k=1)
+			self.count = 0
+			NewGame()	
 
 NewGame()
 
