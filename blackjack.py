2369353c20f3e12d0bc9abaa3442257d4210cb02
Rafael Llopis <rafa94el@gmail.com>
2021-08-26 20:40:22 +0200

    A better version, contained in one class. Needs logic to give user chance to play again or quit

--- blackjack.py
+++ blackjack.py
@@ -4,19 +4,32 @@ dealers_hand = []
 users_budget = 100
 cards = ['A', 'A', 'A', 'A', 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 'J', 'J', 'J', 'J', 'Q', 'Q', 'Q', 'Q', 'K', 'K', 'K', 'K']
 
-
 def NewGame():
-	return input("Your budget is {}. How much are you willing to bet? ".format(users_budget))	
+	while True:
+		user_bet = int(input("Your budget is {}. How much are you willing to bet? ".format(users_budget)))
+		if user_bet <= users_budget:
+			break
+		else:
+			print("Your bet is higher than your budget!")
+			continue
+	global bet
+	bet = user_bet
+	new_game = Game()
+
 
 class Game:
 	
 	def __init__(self):
 		self.users_hand = random.choices(cards, k=2)
 		self.dealers_hand = random.choices(cards, k=1)
+		self.count = 0
+		print(self.users_hand)
+		print(self.dealers_hand)
+		self.evaluate()
 
 	def evaluate(self):
 		self.users_score = 0
-		count = 1
+
 		for i in self.users_hand:
 			if i in ['J', 'Q', 'K']:
 				self.users_score += 10
@@ -27,10 +40,12 @@ class Game:
 					self.users_score += 11
 			else:
 				self.users_score += i
+
 		print("Your score is {}".format(self.users_score))		
+
 		if self.users_score == 21:
-			if count == 1:
-				print("Blackjack! You win {}".format(self.bet * 1.5))
+			if self.count == 1:
+				print("Blackjack! You win {}".format(bet * 1.5))
 				self.users_budget += (bet * 1.5)
 				return
 			else:
@@ -41,6 +56,7 @@ class Game:
 			print("You go bust! You lose {}".format(bet))
 			return
 
+		self.count += 1	
 		self.decide()
 
 	def deal(self):
@@ -56,7 +72,7 @@ class Game:
 
 	def dealers_turn(self):
 		self.dealers_hand.append(random.choice(cards))
-		print("The dealers cards are {}".format(self.users_hand))
+		print("The dealers cards are {}".format(self.dealers_hand))
 		self.evaluate_dealers()
 
 	def evaluate_dealers(self):
@@ -77,7 +93,7 @@ class Game:
 			return
 		elif self.dealers_score <= 16:
 			self.dealers_turn()
-		elif self.dealers_score > 16:
+		elif self.dealers_score > 16 <= 21:
 			if self.dealers_score < self.users_score:
 				print("You win {}".format(bet * 2))
 				return
@@ -88,11 +104,15 @@ class Game:
 			print("The dealer went bust. You win {}".format(bet * 2))
 			return
 
+	def reset(self):
+		self.users_hand = random.choices(cards, k=2)
+		self.dealers_hand = random.choices(cards, k=1)
+		self.count = 0
+		print(self.users_hand)
+		print(self.dealers_hand)
+		NewGame()
 
 
+NewGame()
 
-bet = NewGame()
-new_game = Game()
-print(new_game.users_hand)
-print(new_game.dealers_hand)
-new_game.evaluate()
+
